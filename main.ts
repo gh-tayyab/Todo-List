c254855174be855b6bb06093be04c29de72d7183
Muhammad Tayyab <150431063+gh-tayyab@users.noreply.github.com>
2024-01-24 12:27:40 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+async function main(){
+    let {task} = await inquirer.prompt({
+        type: "input",
+        name: "task",
+        message: "enter your task"
+    })
+    console.log(`added task ${task}`)
+}
+main()
